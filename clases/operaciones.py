230c0810562d74674884e625c7c252a7e43c1bbc
Jorge Ivan Garcia Piña <68978leo@universidaddeleon.edu.mx>
2025-09-19 11:48:02 -0600

    Version multiplicacion 19/09/25 11:47

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -1,15 +1,12 @@
 class Operaciones:
-    
     def __init__(self):
         self.alejandroMontes = "Alejandro Montes"
 
     def saludoAlejandroMontes(self):
         return "Mi nombre es " + self.alejandroMontes
 
-    #Implementar aqui tu metodo
-    '''
-    def operacionAsignada(self):
-        #Realizar la operacion asignada
-    ''' 
+    def multiplica(self, n1, n2):
+        # Realizar la operación asignada
+        return n1 * n2
 
-
+    
