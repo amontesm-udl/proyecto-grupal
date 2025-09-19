c0afbc1df700560711598243f9caf1904e5ac02b
Emmanuel Ibarra <emmanuelibarraruano@gmail.com>
2025-09-19 10:49:38 -0600

    Version 1 - se implemento la funcion minimo(lista) - 19/08/25 10:49

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def main():
     print(test.saludoAlejandroMontes())
     
     #Realiza aqui tu prueba 
-    
+    lista = [5, 3, 9, 1, 4]
+    print("La lista es:", lista)
+    print("El número menor es:", test.minimo(lista))
     
 if __name__ == '__main__':
     main()
