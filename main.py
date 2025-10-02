6c38140f20fcc1cbe918346dfa757fae13fbcf1a
Sergio Ricardo Oviedo Rodriguez <64406leo@universidaddeleon.edu.mx>
2025-10-01 18:35:34 -0600

    feat: lista sin duplicar sergio

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ def main():
     print(test.saludoAlejandroMontes())
     
     # Realiza aqui tu prueba
-    lista = [1,2,2,3,4,5,5,8,9,6,6]
-    print(test.eliminarDuplicados(lista))
+    print(test.eliminarDuplicados([1,2,2,3,4,5,5,8,9,6,6]))
 
 
 if __name__ == '__main__':
